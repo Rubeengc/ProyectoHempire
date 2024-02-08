82f6df06a25dc0b8b19302ac0fbc14a2945aac0e
ruben <ruben@entonografico.myguest.virtualbox.org>
2024-02-08 12:35:54 +0100

    Endpoint Login

--- apphempire/views.py
+++ apphempire/views.py
@@ -5,8 +5,10 @@ from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
 from django.db.models import Count
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import check_password
 import json
-from datetime import datetime
+import jwt
+import datetime
 # Create your views here.
 @csrf_exempt
 def registro(request):
@@ -24,7 +26,7 @@ def registro(request):
     elif nombreusuario == "" or data.get("contraseña") == ""or data.get("repcontraseña")=="" or correoelc == ""  or data.get("nombre") == "" or data.get("apellidos") == "" or data.get("edad") == "": 
         return JsonResponse({"error": "Esta vacio un campo"}, status=201)
 
-    elif request.POST.get("contraseña") !=  request.POST.get("repcontraseña"):
+    elif data.get("contraseña") !=  data.get("repcontraseña"):
         return JsonResponse({ "error":"Las contraseñas no coinciden"})
     elif  queryUsuarios > 0 :
         return JsonResponse({"error":"Este nombre de ya esta registrado"})
@@ -33,24 +35,46 @@ def registro(request):
     elif  queryCorreo > 0 :
         return JsonResponse({"error":"Este correo ya esta de ya esta registrado"})
     else:
-     
-        
-      
         passwd_sinhashear = data.get("contraseña")
         passwd_hashed=make_password(passwd_sinhashear)
         Nuevousuario= Usuarios(username=nombreusuario ,contraseña=passwd_hashed,correo= correoelc,edad=edadobli,nombre= data.get("nombre"),apellidos=data.get("apellidos"),fechareg=fecharegistro )
         Nuevousuario.save()
     return JsonResponse({"Estado":"Registrado correctamente"})
 
-       
+
+
 @csrf_exempt
-def login_logout(request):
-  if request.method == "PATCH":
-    token = request.headers.get("SessionToken", None)
-    if token is None:
-      return JsonResponse({"error": "Missing Token"}, status=401)
-    return JsonResponse({"status": "success"})
-  elif request.method == "POST":
-    return JsonResponse({"sessionToken": "abc"}, status=201)
+def generar_jwt_token(request,id):
+  payload = {
+    'id' : id,
+    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1), 
+    'iat': datetime.datetime.utcnow(),
+  }
+  token = jwt.encode(payload, 'clave_authz', algorithm='HS256')
+  return token
+
+
+@csrf_exempt
+def login(request):
+  if request.method != "PATCH":
+    return JsonResponse({"error": "Metodo HTTP no soportado"}, status=405) 
   else:
-    return JsonResponse({"error": "No mandaste un PATCH ni un POST"}, status=405)
+    data = json.loads(request.body)
+    nombreusuarioenv =  data.get("username") 
+    contraseñaenv= data.get("contraseña")
+    print(nombreusuarioenv)
+    print(contraseñaenv)
+    queryUser =Usuarios.objects.get(username=nombreusuarioenv)
+    contraseñaguardada =queryUser.contraseña
+    print(contraseñaguardada)
+    if queryUser == None:
+        return JsonResponse({"error": "No estas registrado"}, status=405)
+    else:
+      if check_password(contraseñaenv,contraseñaguardada):
+        tokenn=generar_jwt_token(request,queryUser.id)
+        queryUser.token = tokenn
+        queryUser.save()
+        return JsonResponse({"token":tokenn})
+      else:
+        return JsonResponse({"error": "La contraseña no es correcta"}, status=405)
+
--- proyectohempire/urls.py
+++ proyectohempire/urls.py
@@ -20,5 +20,6 @@ from apphempire import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registro', views.registro)
+    path('registro', views.registro),
+    path('login', views.login)
 ]
